df1b60244df62b91ef19ae591183a0b94556d109
mrivs <valentinilyins@gmail.com>
2022-11-26 01:47:50 +0300

    program added

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] array = PrintStrAndGetArray("Введите массив (через пробел)");
+
+int length = array.Length;
+int size = 0;
+for (int i = 0; i < length; i++)
+{
+    if (array[i].Length < 4) size++;
+}
+if (size > 0)
+{
+    string[] newarray = new string[size];
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newarray[count] = array[i];
+            count++;
+        }
+    }
+Console.WriteLine(PrintArray(newarray));
+}
+else Console.WriteLine("[]");
+
+
+string PrintArray(string[] array) // пишем массив в строку
+{
+    string str = "[" + string.Join(", ", array) + "]";
+    return str;
+}
+
+string[] PrintStrAndGetArray(string message) // пишем сообщение и задаем массив строк
+{
+    Console.WriteLine(message);
+
+    string inputString = (Console.ReadLine());
+    string[] array = inputString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return array;
+}
